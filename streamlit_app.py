fcbaedec13307b8bc6f2e8dfe2294123cabe999c
blacktech589-cyber <blacktech589@gmail.com>
2026-07-04 10:47:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,11 +35,11 @@ try:
     XGB_OK = True
 except Exception:
     XGB_OK = False
-   try:
-    from lightgbm import LGBMClassifier
-    LGBM_OK = True
-except Exception:
-    LGBM_OK = False
+    try:
+        from lightgbm import LGBMClassifier
+        LGBM_OK = True
+    except Exception:
+        LGBM_OK = False
 
 # Data Generation
 from sklearn.datasets import make_classification
